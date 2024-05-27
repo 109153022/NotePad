33912be6be3dbcb0a054c6701f1af54d0b42c326
109153022 <109153022@gas.fgu.edu.tw>
2024-05-27 14:18:21 +0800

    5/27進度
    完成
    儲存檔案
    復原上一步
    復原下一步
    字體
    RTF格式

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,12 @@ namespace NotePad
             this.rtbText = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listUndo = new System.Windows.Forms.ListBox();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnRedo = new System.Windows.Forms.Button();
+            this.comboBoxFont = new System.Windows.Forms.ComboBox();
+            this.comboBoxStyle = new System.Windows.Forms.ComboBox();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnOpen
@@ -48,7 +54,7 @@ namespace NotePad
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(146, 33);
+            this.btnSave.Location = new System.Drawing.Point(135, 33);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(117, 54);
             this.btnSave.TabIndex = 1;
@@ -59,19 +65,77 @@ namespace NotePad
             // 
             this.rtbText.Location = new System.Drawing.Point(12, 93);
             this.rtbText.Name = "rtbText";
-            this.rtbText.Size = new System.Drawing.Size(731, 345);
+            this.rtbText.Size = new System.Drawing.Size(505, 340);
             this.rtbText.TabIndex = 2;
             this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listUndo
+            // 
+            this.listUndo.FormattingEnabled = true;
+            this.listUndo.ItemHeight = 12;
+            this.listUndo.Location = new System.Drawing.Point(534, 93);
+            this.listUndo.Name = "listUndo";
+            this.listUndo.Size = new System.Drawing.Size(254, 340);
+            this.listUndo.TabIndex = 3;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(258, 33);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(117, 54);
+            this.btnUndo.TabIndex = 4;
+            this.btnUndo.Text = "恢復上一步";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // btnRedo
+            // 
+            this.btnRedo.Location = new System.Drawing.Point(381, 33);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(117, 54);
+            this.btnRedo.TabIndex = 5;
+            this.btnRedo.Text = "恢復下一步";
+            this.btnRedo.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxFont
+            // 
+            this.comboBoxFont.FormattingEnabled = true;
+            this.comboBoxFont.Location = new System.Drawing.Point(12, 7);
+            this.comboBoxFont.Name = "comboBoxFont";
+            this.comboBoxFont.Size = new System.Drawing.Size(149, 20);
+            this.comboBoxFont.TabIndex = 6;
+            // 
+            // comboBoxStyle
+            // 
+            this.comboBoxStyle.FormattingEnabled = true;
+            this.comboBoxStyle.Location = new System.Drawing.Point(349, 7);
+            this.comboBoxStyle.Name = "comboBoxStyle";
+            this.comboBoxStyle.Size = new System.Drawing.Size(149, 20);
+            this.comboBoxStyle.TabIndex = 7;
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Location = new System.Drawing.Point(184, 7);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(149, 20);
+            this.comboBoxSize.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxSize);
+            this.Controls.Add(this.comboBoxStyle);
+            this.Controls.Add(this.comboBoxFont);
+            this.Controls.Add(this.btnRedo);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.listUndo);
             this.Controls.Add(this.rtbText);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnOpen);
@@ -88,6 +152,12 @@ namespace NotePad
         private System.Windows.Forms.RichTextBox rtbText;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listUndo;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnRedo;
+        private System.Windows.Forms.ComboBox comboBoxFont;
+        private System.Windows.Forms.ComboBox comboBoxStyle;
+        private System.Windows.Forms.ComboBox comboBoxSize;
     }
 }
 
